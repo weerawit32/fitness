980881d31f827d04f25102bbfa6d6ea27f0ee312
weerawit32 <vasthabo@hotmail.com>
2022-08-30 13:18:59 +0700

    unneeded fetchById

---
+++ fitness/src/utils/fetchById.ts
@@ -0,0 +1,33 @@
+// import axios from "axios";
+export {};
+
+// type exerciseOptionsType = {
+//   headers: {
+//     "X-RapidAPI-Key": string;
+//     "X-RapidAPI-Host": string;
+//   };
+// };
+
+// export type innerData = {
+//   bodyPart: string;
+//   equipment: string;
+//   gifUrl: string;
+//   id: string;
+//   name: string;
+//   target: string;
+// };
+
+// export type allData = innerData[];
+
+
+// export fetchById ( url: string,
+//     exerciseOptions: exerciseOptionsType) {
+//     return new Promise<innerData>(async (resolve, reject) => {
+//       try {
+//         const { data } = await axios.get(url, exerciseOptions);
+//         return resolve(data);
+//       } catch (error) {
+//         reject(error);
+//       }
+//     });
+//   })
